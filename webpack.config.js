9ae8629b2c6f9e32d006265e7e135caf0b299331
Mike Papale <mpapale@splunk.com>
2016-02-04 12:48:53 -0800

    Do the Hot Module Replacement. So Hot

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const merge = require('webpack-merge');
+const webpack = require('webpack');
 
 const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
@@ -18,7 +19,30 @@ const common = {
 };
 
 if (TARGET === 'start' || !TARGET) {
-	module.exports = merge(common, {});
+	module.exports = merge(common, {
+		devServer: {
+			contentBase: PATHS.build,
+
+			// Enable history API fallback so HTML5 History API based
+			// routing works. This is a good default that will come
+			// in handy in more complicated setups.
+			// mpapale: What?
+			historyApiFallback: true,
+			hot: true,
+			inline: true,
+			progress: true,
+
+			// Display only errors to reduce the amount of output
+			stats: 'errors-only',
+
+			// Parse host and port from env so this is easy to customize.
+			host: process.env.HOST,
+			port: process.env.PORT
+		},
+		plugins: [
+			new webpack.HotModuleReplacementPlugin()
+		]
+	});
 }
 
 if (TARGET === 'build') {
