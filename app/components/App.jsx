e147e2fb466f3f2e1b08efcdb93d26bde2ded787
Mike Papale <mpapale@splunk.com>
2016-02-11 12:22:02 -0800

    Use helpful AltContainer to do the bindings

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,3 +1,4 @@
+import AltContainer from 'alt-container';
 import React from 'react';
 import Notes from './Notes.jsx';
 
@@ -5,39 +6,24 @@ import NoteActions from '../actions/NoteActions';
 import NoteStore from '../stores/NoteStore';
 
 export default class App extends React.Component {
-	constructor(props) {
-		super(props);
-
-		this.state = NoteStore.getState();
-	}
-
-	componentDidMount() {
-		NoteStore.listen(this.storeChanged);
-	}
-
-	componentWillUnmount() {
-		NoteStore.unlisten(this.storeChanged);
-	}
-
-	storeChanged = (state) => {
-		// Without a property initializer, `this` wouldn't
-		// point at the right context because it defaults to 
-		// `undefined` in strict mode.
-		this.setState(state);
-	};
-
 	render() {
-		const notes = this.state.notes;
-
 		return (
 			<div>
 				<button className="add-note" 
 					onClick={this.addNote}>+</button>
-				{/* this is a comment */}
-				<Notes notes={notes} 
-					onEdit={this.editNode}
-					onDelete={this.deleteNote}
-				/>
+				{/* AltContainer wires up all the
+				  * bindings for us!
+				  */}
+				<AltContainer
+					stores={[NoteStore]}
+					inject={{
+						notes: () => NoteStore.getState().notes
+					}}
+				>
+					<Notes 
+						onEdit={this.editNode} 
+						onDelete={this.deleteNote} />
+				</AltContainer>
 			</div>
 		);
 	}
